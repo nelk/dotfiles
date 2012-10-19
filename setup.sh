387533150caa89c6414e49851ab434269b509878
nelk <nelkishere@gmail.com>
2012-10-19 14:49:47 -0700

    Removed .vim directory, will add more vim plugins into janus as submodules as required. Added oh-my-zsh and updated setup script.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo 'Setting up oh-my-zsh'
+./oh-my-zsh/oh-my-zsh.sh
+
+echo 'Setting up janus'
 ./janus/bootstrap.sh
+
+echo 'Building sym links'
+ln ./.zshrc ~/.zshrc
+ln ~/.bash_profile ./oh-my-zsh/custom/bash_profile.zsh
 ln ./.vimrc.after ~/.vimrc.after
 
