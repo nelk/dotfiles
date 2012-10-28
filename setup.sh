2db83edf8ed4c44d68f832f284cad9a9e012ec4b
nelk <nelkishere@gmail.com>
2012-10-28 14:23:47 -0700

    Instead of sym linking .bashrc or .bash_profile files into oh-my-zsh custom folder, just source them in bashrc_globals to control order of execution. Can customize and set VIMPATH in .bashrc which is used to setup aliases and functions.

--- setup.sh
+++ setup.sh
@@ -17,12 +17,6 @@ ln -s $DOTFILES/.zshrc ~/.zshrc
 ln -s $DOTFILES/.vimrc.after ~/.vimrc.after
 ln -s $DOTFILES/.janus ~/.janus
 
-# Sym links for zsh to run
+# Sym link for zsh to run
 ln -s $DOTFILES/bashrc_global $DOTFILES/oh-my-zsh/custom/bashrc_global.zsh
-if [ -e ~/.bash_profile ]; then
-  ln -s ~/.bash_profile $DOTFILES/oh-my-zsh/custom/bash_profile.zsh
-fi
-if [ -e ~/.bashrc ]; then
-  ln -s ~/.bashrc $DOTFILES/oh-my-zsh/custom/bashrc.zsh
-fi
 
