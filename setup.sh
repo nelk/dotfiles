d441c29593f211591cf1e8fab89f7c862607d1fc
nelk <nelkishere@gmail.com>
2015-02-18 18:49:38 -0800

    Fix setup script to make required directories.

--- setup.sh
+++ setup.sh
@@ -12,14 +12,17 @@ echo 'Building sym links'
 ln -s $DOTFILES/.zshrc ~/.zshrc
 ln -s $DOTFILES/.i3 ~/.i3
 ln -s $DOTFILES/.xinitrc ~/.xinitrc
+ln -s $DOTFILES/.xinitrc ~/.xsession
 ln -s $DOTFILES/.gitignore ~/.gitignore
 ln -s $DOTFILES/.vimrc ~/.vimrc
+mkdir -p ~/.vim
 ln -s $DOTFILES/bundle ~/.vim/bundle
 ln -s $DOTFILES/autoload ~/.vim/autoload
 ln -s $DOTFILES/colors ~/.vim/colors
 ln -s $DOTFILES/syntax ~/.vim/syntax
 
 # Powerline
+mkdir -p ~/.config
 ln -s $DOTFILES/powerline/powerline/powerline_config ~/.config/powerline
 ln -s $DOTFILES/powerline/powerline/bindings/vim $DOTFILES/bundle/vim-powerline
 
@@ -27,6 +30,7 @@ ln -s $DOTFILES/powerline/powerline/bindings/vim $DOTFILES/bundle/vim-powerline
 ln -s $DOTFILES/bashrc_global $DOTFILES/oh-my-zsh/custom/bashrc_global.zsh
 
 # Xmonad
+mkdir -p ~/.xmonad
 ln -s $DOTFILES/xmonad.hs ~/.xmonad/xmonad.hs
 ln -s $DOTFILES/.xmobarrc ~/.xmobarrc
 
